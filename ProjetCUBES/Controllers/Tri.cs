95bb589da4f63a775d4783ed889aa9fb71aeff3a
Bada_Safwane <safwaneb1@gmail.com>
2023-01-05 16:10:39 +0100

    creation controler Tri

---
+++ ProjetCUBES/Controllers/Tri.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Faker;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjetCUBES.Model;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using static ProjetCUBES.Helpers.Class;
+namespace ProjetCUBES.Controllers
+{
+
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+
+    public class Tri
+    {
+
+
+        [HttpGet]
+        public List<Article> displayarticlebyfamily(int ID)
+        {
+            using (Apply context = new Apply())
+            {
+                List<Article> article = context.Articles.Where(x => x.IdFamily == ID).ToList();
+                return article;
+            }
+        }
+
+        public List<Article> displayarticlebySupplier(int IDSupplier)
+        {
+            using (Apply context = new Apply())
+            {
+                List<Article> article = context.Articles.Where(x => x.IdProvider == IDSupplier).ToList();
+                return article;
+            }
+        }
+    }
+}
